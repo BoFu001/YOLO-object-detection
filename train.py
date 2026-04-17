43bc6b1a97428d1f0e05877700ecb579564a267a
BoFu001 <bofu001@gmail.com>
2026-04-17 16:50:24 +0100

    Add reproducibility seed settings to train.py

--- train.py
+++ train.py
@@ -3,10 +3,20 @@
 # Run: python train.py
 
 import os
+import random
 import argparse
+import numpy as np
 import torch
 
 from config import *
+
+# reproducibility
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 # dataloader
 from modules.Dataset import get_dataloaders
